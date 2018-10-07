5c15f1467ccb237d8cfc8cf2229471f5ea43e8d9
qofo1 <43939837+qofo1@users.noreply.github.com>
2018-10-08 00:24:25 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include_once'config/init.php'; ?>
+
+
+
+<?php
+
+$job = new Job;
+    
+$template = new Template('templates/frontpage.php');
+
+$template -> title = 'Latest job offer';
+$template->jobs =  $job->getAllJobs();
+
+ echo $template; 
+
